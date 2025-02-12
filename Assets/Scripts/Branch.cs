acb7e59693db85ec09aa03caa7811455a1cea26b
Gadjeid <adjeidarkogd@gmail.com>
2025-02-11 19:45:03 -0600

    Fix to missed branch logic
    fixed missed branch logic so round doesn't end when missing a branch

--- Assets/Scripts/Branch.cs
+++ Assets/Scripts/Branch.cs
@@ -5,15 +5,11 @@ using System.Collections.Generic;
 public class Branch : MonoBehaviour
 {
     public static float bottomY = -18f;
-    private bool hasTriggeredMiss = false;
 
     void Update()
     {
-        if (!hasTriggeredMiss && transform.position.y < bottomY)
+        if (transform.position.y < bottomY)
         {
-            hasTriggeredMiss = true;
-            ApplePicker apScript = Camera.main.GetComponent<ApplePicker>();
-            apScript.AppleMissed();
             Destroy(this.gameObject);
         }
     }
